8063aa4477ba6e1e7683ae7482b1b42247fff0ea
Nikolay Napadaylo <nn@MacBook-Pro-Nikolay.local>
2023-02-22 21:17:28 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './main.scss';
+import './fonts/fonts.scss';
+// import './images/cart.png';
+// import './images/cart.svg';
 
-function component(text) {
-    const element = document.createElement('h1');
-    element.textContent = text;
-    return element;
-  }
+// function component(text) {
+//     const element = document.createElement('h1');
+//     element.textContent = text;
+//     return element;
+//   }
   
-  document.body.prepend(component('Проект собран на Webpack'));
+//   document.body.prepend(component('Проект собран на Webpack'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path: path.join(__dirname, "dist"),
     filename: "index.[contenthash].js",
+    // assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
   },
   module: {
     rules: [
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
       {
         test: /\.(scss|css)$/,
         use: [
@@ -20,9 +26,28 @@ module.exports = {
           "sass-loader",
         ],
       },
+      // {
+      //   test: /\.(png|jpg|jpeg|gif)$/i,
+      //   type: "asset/resource",
+      // },
+      // {
+      //   test: /\.svg$/,
+      //   type: "asset/resource",
+      //   generator: {
+      //     filename: path.join("icons", "[name].[contenthash][ext]"),
+      //   },
+      // },
     ],
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/images"),
+          to: path.resolve(__dirname, "dist/images"),
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "template.html"),
       filename: "index.html",
